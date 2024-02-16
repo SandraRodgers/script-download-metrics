98401e19848de96762c0519396002ff3257f0fd0
Sandra Rodgers <sandra.rodgers@deepgram.com>
2024-02-15 20:55:35 -0600

    update packages

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import os
 import glob
 import pandas as pd
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 
 load_dotenv()
